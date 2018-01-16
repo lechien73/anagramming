44c79648e314a54b41967778f223c3e29c9a8b0b
Matt Rudge <mrudge@gmail.com>
2018-01-16 15:17:05 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Simple Anagram Maker
+
+A simple one-word anagram generator for Python 3, created using no external libraries.
+
+Type in the letters you wish to check. If it's possible for one or more single word anagram from those letters, then they will be printed to the screen.
+
+## Credits
+
+Using the wordlist from http://www.keithv.com/software/wlist/
---
+++ anagram.py
@@ -0,0 +1,26 @@
+def get_dictionary(dictionary_file):
+    with open(dictionary_file) as f:
+        return [w.strip().lower() for w in f]
+
+def count_letters(word):
+    counts = {}
+    for letter in word:
+        counts[letter] = counts.get(letter, 0) + 1
+    return counts
+
+def anagram(a, b):
+    print("Anagramming %s" % a)
+    anagrams = []
+    for word in b:
+        if len(word) == len(a) and count_letters(a.lower()) == count_letters(word):
+            anagrams.append(word.lower())
+    return anagrams if anagrams else ["No single word anagrams found"]        
+
+mydict = get_dictionary('words.txt')
+
+print("Single Word Anagrammer")
+
+word = input("Enter a string: ")
+
+for anagram in anagram(word, mydict):
+    print(anagram)
