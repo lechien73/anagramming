84f2a45230dc52f4e904186edc1735960b75b13b
Matt Rudge <mrudge@gmail.com>
2018-01-16 16:42:35 +0000

    Cosmetic changes

--- anagram.py
+++ anagram.py
@@ -33,7 +33,7 @@ alphabet = "abcdefghijklmnopqrstuvwxyz"
 mydict = get_dictionary('words.txt')
 
 print("Single Word Anagrammer")
-print("You can use one question mark (?) as a wildcard.")
+print("You can use one question mark (?) as a wildcard. Additional wildcards will be ignored.")
 print("")
 
 word = input("Enter a string: ")
